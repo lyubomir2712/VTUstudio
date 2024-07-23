a47f733b4bc79cebcc09c3f2297a334ebf9bf5a7
Lyubomir Georgiev <lyubomirgeorgiev@192.168.100.2>
2024-07-23 11:03:16 +0300

    Added a navbar and background image

---
+++ WebDevelopmentAgencyApp/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import './Navbar.css'
+export default function Navbar() {
+    return (
+        <header>
+        <nav className="navbar">
+                <span>Български</span>
+                <ul className="nav-list">
+                    <li className="nav-item"><a href="#">Web design</a></li>
+                    <li className="nav-item"><a href="#">Print advertising</a></li>
+                    <li className="nav-item"><a href="#">Photography</a></li>
+                    <li className="nav-item"><a href="#">Portfolio</a></li>
+                    <li className="nav-item"><a href="#">Photoalbums</a></li>
+                    <li className="nav-item"><a href="#">Contacts</a></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
