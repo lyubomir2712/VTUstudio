179dc4c1179752423c2547e01dc209994057e105
Lyubomir Georgiev <lyubomirgeorgiev@192.168.100.2>
2024-07-23 18:21:29 +0300

    Added main section with some svgs, also updated the background

--- WebDevelopmentAgencyApp/src/Components/Main/Main.jsx
+++ WebDevelopmentAgencyApp/src/Components/Main/Main.jsx
@@ -10,8 +10,78 @@ export default function Main() {
             <img src={GearSvg} alt="Gear" className="rotating-gear"/>
             <img src={GearSvg} alt="Gear" className="rotating-gear"/>
             <img src={GearSvg} alt="Gear" className="rotating-gear"/>
+            <img src={GearSvg} alt="Gear" className="rotating-gear"/>
 
+            <svg viewBox="0 0 500 100" className="svgJS" xmlns="http://www.w3.org/2000/svg">
+                <text
+                    x="50%"
+                    y="50%"
+                    dominantBaseline="middle"
+                    textAnchor="middle"
+                    fontSize="60"
+                    fontFamily="Arial, sans-serif"
+                    fill="black"
+                    stroke="black"
+                    strokeWidth="3"
+                    strokeDasharray="100 100"
+                    strokeDashoffset="100"
+                    id="sql-text">
+                    JS
+                </text>
+            </svg>
 
+            <svg viewBox="0 0 500 100" className="svgC#" xmlns="http://www.w3.org/2000/svg">
+                <text
+                    x="50%"
+                    y="50%"
+                    dominantBaseline="middle"
+                    textAnchor="middle"
+                    fontSize="60"
+                    fontFamily="Arial, sans-serif"
+                    fill="black"
+                    stroke="black"
+                    strokeWidth="3"
+                    strokeDasharray="100 100"
+                    strokeDashoffset="100"
+                    id="sql-text">
+                    C#
+                </text>
+            </svg>
+
+            <svg viewBox="0 0 500 100" className="svgSQL" xmlns="http://www.w3.org/2000/svg">
+                <text
+                    x="50%"
+                    y="50%"
+                    dominantBaseline="middle"
+                    textAnchor="middle"
+                    fontSize="60"
+                    fontFamily="Arial, sans-serif"
+                    fill="black"
+                    stroke="black"
+                    strokeWidth="3"
+                    strokeDasharray="100 100"
+                    strokeDashoffset="100"
+                    id="sql-text">
+                    SQL
+                </text>
+            </svg>
+            <svg viewBox="0 0 500 100" className="svgHTML" xmlns="http://www.w3.org/2000/svg">
+                <text
+                    x="50%"
+                    y="50%"
+                    dominantBaseline="middle"
+                    textAnchor="middle"
+                    fontSize="60"
+                    fontFamily="Arial, sans-serif"
+                    fill="black"
+                    stroke="black"
+                    strokeWidth="3"
+                    strokeDasharray="100 100"
+                    strokeDashoffset="100"
+                    id="sql-text">
+                    HTML
+                </text>
+            </svg>
         </>
     )
 }
