62a0450cb845cd6aa0577bc489172b7e00afd124
Lyubomir Georgiev <lyubomirgeorgiev@Lyubomirs-MacBook-Pro.local>
2024-07-25 10:22:35 +0300

    Worked on the navbar and main component

--- WebDevelopmentAgencyApp/src/Components/Navbar/Navbar.jsx
+++ WebDevelopmentAgencyApp/src/Components/Navbar/Navbar.jsx
@@ -1,17 +1,22 @@
 import './Navbar.css'
+import Logo from '../../assets/VTUstudioLogo.png'
 export default function Navbar() {
     return (
         <header>
             <nav className="navbar">
                 <p>Български</p>
-                <ul className="nav-list">
-                    <li className="nav-item"><a href="#">Web design</a></li>
-                    <li className="nav-item"><a href="#">Print advertising</a></li>
-                    <li className="nav-item"><a href="#">Photography</a></li>
-                    <li className="nav-item"><a href="#">Portfolio</a></li>
-                    <li className="nav-item"><a href="#">Photoalbums</a></li>
-                    <li className="nav-item"><a href="#">Contacts</a></li>
-                </ul>
+
+                    <img src={Logo} className={"logo"} alt="Logo" />
+                    <span>Studio</span>
+                    <ul className="nav-list">
+                        <li className="nav-item"><a href="#">Web design</a></li>
+                        <li className="nav-item"><a href="#">Print advertising</a></li>
+                        <li className="nav-item"><a href="#">Photography</a></li>
+                        <li className="nav-item"><a href="#">Portfolio</a></li>
+                        <li className="nav-item"><a href="#">Photoalbums</a></li>
+                        <li className="nav-item"><a href="#">Contacts</a></li>
+                    </ul>
+
             </nav>
         </header>
     )
