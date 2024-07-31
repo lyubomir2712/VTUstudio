501e2af4f1a6fa2a72b90648d0455f350b1c8e7d
lyubomir2712 <lyubomirgeorgiev2712@gmail.com>
2024-07-31 16:05:10 +0300

    added sesctionThree jsx and css files

--- VTUstudio/src/Components/SectionOne/SectionOne.jsx
+++ VTUstudio/src/Components/SectionOne/SectionOne.jsx
@@ -38,23 +38,7 @@ export default function SectionOne() {
                 <img src={lemon} alt="lemon logo"/>
             </motion.div>
 
-            <motion.div
-                initial={{x: -1000}}   // Start 1000px to the left of the neutral position
-                animate={{x: 0}}       // Animate to the neutral position
-                transition={{duration: 1, ease: 'easeOut'}}  // Smooth animation
-                style={{
-                    width: '500px',
-                    height: '500px',
-                    margin: '0 auto', // Centers the div horizontally within its parent
-                    display: 'flex',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    fontWeight: 'bold',
-                    padding: '0',
-                }}
-            >
-                <img src={peacockImg} alt="peacock logo"/>
-            </motion.div>
+
 
 
             <motion.div
